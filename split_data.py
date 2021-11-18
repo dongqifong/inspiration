03fb662715a9b8fcb21598eafeace9c75cc4631a
qifong <qifong04@gmail.com>
2021-11-19 00:32:23 +0800

    update split_data

--- split_data.py
+++ split_data.py
@@ -16,5 +16,16 @@ def split_his_furture(arr, his_win, fur_win, slide_win):
     for t in range(t_start, t_end, slide_win):
         his.append(arr[t-his_win:t, :])
         future.append(arr[t:t+fur_win, :])
-
     return np.array(his), np.array(future)
+
+
+def train_test_split(x, y=None, train_size=0.66, random_state=0, shuffle=True):
+    from sklearn.model_selection import train_test_split
+    if y is not None:
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, train_size=train_size, random_state=random_state, shuffle=shuffle)
+        return x_train, x_test, y_train, y_test
+    else:
+        x_train, x_tset = train_test_split(
+            x, train_size=train_size, random_state=random_state, shuffle=shuffle)
+        return x_train, x_tset
